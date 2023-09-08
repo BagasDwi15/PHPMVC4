d43beeac80e25311a90346ca28e9e4d3a485ad11
BagasDwi15 <144404435+BagasDwi15@users.noreply.github.com>
2023-09-08 13:14:03 +0700

    Add files via upload

---
+++ phpmvc/app/view/User/profile.php
@@ -0,0 +1,6 @@
+<div class="container text-center mt-4">
+    <h1>Halaman Saya</h1>
+    <img src="http://localhost/phpmvc/img/00.jpg" alt="bagas" width="200" class="rounded-circle shadow mb-2">
+    <p>Hallo, Nama saya <?= $data['nama']?>, Saya seorang <?=$data['pekerjaan']?></p>
+
+</div>
